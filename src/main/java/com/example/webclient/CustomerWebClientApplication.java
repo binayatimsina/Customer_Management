0e158ced1ac22934133ab400f1ced1e99bbb5cda
binayatimsina <binayatimsina2@gmail.com>
2024-08-06 18:21:48 +0000

    Create initial files

---
+++ src/main/java/com/example/webclient/CustomerWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerWebClientApplication.class, args);
+	}
+
+}
