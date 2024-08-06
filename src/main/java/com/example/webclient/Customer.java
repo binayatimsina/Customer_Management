6d98fa31ec738d64db59f8890086599801c5244f
Utkarsh <utkarshnsr@gmail.com>
2024-08-06 18:55:36 +0000

    add customer class

---
+++ src/main/java/com/example/webclient/Customer.java
@@ -0,0 +1,27 @@
+package com.example.webclient;
+
+public class Customer {
+    private Long id;
+    private String name;
+    
+    public Customer(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
