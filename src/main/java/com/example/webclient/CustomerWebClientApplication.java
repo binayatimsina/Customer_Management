8cf1dcb237459fbc98234bca80e1bd7142d39b2d
Utkarsh <utkarshnsr@gmail.com>
2024-08-06 19:27:37 +0000

    add resttemplate bean

--- src/main/java/com/example/webclient/CustomerWebClientApplication.java
+++ src/main/java/com/example/webclient/CustomerWebClientApplication.java
@@ -2,9 +2,16 @@ package com.example.webclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CustomerWebClientApplication {
+
+	@Bean 
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerWebClientApplication.class, args);
