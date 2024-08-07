0eeb31022bc623976bf42271d256b92bf2a7e141
binayatimsina <binayatimsina2@gmail.com>
2024-08-07 15:36:17 +0000

    Rest Tempelate get method

--- src/main/java/com/example/webclient/CustomerWebController.java
+++ src/main/java/com/example/webclient/CustomerWebController.java
@@ -8,13 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
+import com.example.webclient.Customer;
 
 @Controller
 @RequestMapping("/web/customers")
 public class CustomerWebController {
-<<<<<<< HEAD
     @Autowired RestTemplate restTemplate;
     private final String url = "http://localhost:8080/customers";
     @GetMapping("")
@@ -22,41 +23,37 @@ public class CustomerWebController {
         Customer[] customers = restTemplate.getForObject(url, Customer[].class);
         System.out.println(customers.getClass());
         model.addAttribute("customers", customers);
-=======
-    @Autowired 
-    private RestTemplate restTemplate;
-
-    @GetMapping
-    public String getAllCustomers(Model model) {
-        Customer[] customer = restTemplate.getForObject("http://localhost:8080/customers", Customer[].class);
-        model.addAttribute("customers", customer);
->>>>>>> de9e2af6753b7101c25c9f60eda378608a73638b
         return  "customers";
     }
     
-    // public void createCustomerForm() {
-    //     restTemplate.getForObject("http://localhost:8080/customers", String.class);
-    // }
+    @GetMapping("/create")
+    public String createCustomerForm(Model model) {
+        model.addAttribute("customer", new Customer(null, null));
+        return "customer-form";
+    }
 
-    // public String getCustomer(Long id) {
-    //     return restTemplate.getForObject("http://localhost:8080/customers/" + id, String.class);
-    // }
-    // public String createCustomer() {
-    //     return restTemplate.postForObject("http://localhost:8080/customers", null, String.class);
-    // }
 
-    // public String editCustomerForm(Long id) {
-    //     return restTemplate.getForObject("http://localhost:8080/customers/" + id , String.class);
-    // }
+    public String getCustomer(Long id) {
+        return restTemplate.getForObject("http://localhost:8080/customers/" + id, String.class);
+    }
 
-    // public String updateCustomer() {
-    //     restTemplate.put("http://localhost:8080/customers/1", null);
-    //     return "Customer updated";
-    // }
+    @PostMapping("")
+    public String createCustomer() {
+        return restTemplate.postForObject("http://localhost:8080/customers", null, String.class);
+    }
 
-    // public String deleteCustomer(Long id) {
-    //     restTemplate.delete("http://localhost:8080/customers/" + id);
-    //     return "Customer deleted";
-    // }
+    public String editCustomerForm(Long id) {
+        return restTemplate.getForObject("http://localhost:8080/customers/" + id , String.class);
+    }
+
+    public String updateCustomer() {
+        restTemplate.put("http://localhost:8080/customers/1", null);
+        return "Customer updated";
+    }
+
+    public String deleteCustomer(Long id) {
+        restTemplate.delete("http://localhost:8080/customers/" + id);
+        return "Customer deleted";
+    }
 
 }
