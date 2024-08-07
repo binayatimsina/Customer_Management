b2df2da86c3275a59db101e1362f3c4df1bb6028
binayatimsina <binayatimsina2@gmail.com>
2024-08-07 19:26:58 +0000

    Put method implement

--- src/main/java/com/example/webclient/CustomerWebController.java
+++ src/main/java/com/example/webclient/CustomerWebController.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import com.example.webclient.Customer;
@@ -45,13 +47,18 @@ public class CustomerWebController {
         return "redirect:customers";
     }
 
-    public String editCustomerForm(Long id) {
-        return restTemplate.getForObject("http://localhost:8080/customers/" + id , String.class);
+    @GetMapping("/edit/{id}")
+    public String editCustomerForm(@PathVariable Long id, Model model) {
+        Customer customer = restTemplate.getForObject(url + "/" + id, Customer.class);
+        model.addAttribute("customer", customer);
+        return "customer-form";
     }
 
-    public String updateCustomer() {
-        restTemplate.put("http://localhost:8080/customers/1", null);
-        return "Customer updated";
+    @PostMapping("/update/{id}")
+    public String updateCustomer(@PathVariable Long id, @ModelAttribute Customer customer) {
+        restTemplate.put(url + "/" + id, customer); 
+        return "redirect:/web/customers";
+
     }
 
     public String deleteCustomer(Long id) {
